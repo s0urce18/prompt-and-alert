12e005771361266a8eb60139a714bbc232e27c03
s0urce18 <54136752+s0urce18@users.noreply.github.com>
2022-04-27 12:37:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const decoder = new StringDecoder('utf8'); // creating object StringDecoder
 function getChar() { // function for getting read character's code
     const buf = new Buffer.alloc(1, 0, 'utf8'); // creating object Buffer
     let bytesRead = fs.readSync(fd, buf, 0, 1); // reading adn saving length of read bytes
-    if (bytesRead > 0) return decoder.write(buf).charCodeAt(0); // checking is the char read and return it's code in possitive way
+    if (bytesRead > 0) return decoder.write(buf).charCodeAt(0); // checking is the char read and return it's code in positive way
     return -1; // return if char isn't read
 }
 
 function readLine(sep = "") { // function for reading line with separating by sep
     let s = "", char, c, end = "\r\n";
-    while ((c = getChar()) != -1 && end.indexOf(char = String.fromCharCode(c)) != -1); // moving reading start to firtst not \r or \n  char
-    if (c != -1) s = char; // firtst char
+    while ((c = getChar()) != -1 && end.indexOf(char = String.fromCharCode(c)) != -1); // moving reading start to first not \r or \n  char
+    if (c != -1) s = char; // first char
     while ((c = getChar()) != -1 && end.indexOf(char = String.fromCharCode(c)) == -1) { // reading chars while end char isn't found
         s += char; // adding chars in one string while they aren't run out
     }
@@ -21,7 +21,7 @@ function readLine(sep = "") { // function for reading line with separating by se
 }
 
 function alert(msg = "", withoutN = false) { // function for writing messages, withoutN - will output be checked on having \n at the end
-    if (!withoutN && msg != '' && msg[msg.length - 1] != '\n') msg += '\n'; // if withoutM == true and last char of msg not \n add \n
+    if (!withoutN && msg != '' && msg[msg.length - 1] != '\n') msg += '\n'; // if withoutN == true and last char of msg not \n add \n
     fs.writeSync(process.stdout.fd, msg.toString()); // writing
 }
 
